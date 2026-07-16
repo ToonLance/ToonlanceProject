be910adb9f42360a5cbffb8e078ff31f6cd1bc78
chirag agrawal <chiragagrawal1264@gmail.com>
2026-07-16 20:24:11 +0530

    name change

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -143,7 +143,7 @@ useEffect(() => {
         href="/"
        className="text-2xl md:text-3xl font-bold tracking-wide"
       >
-        Toon<span className="text-purple-500">lance</span>
+        chirag<span className="text-purple-500">Agrawal</span>
       </Link>
 
       {/* Desktop Links */}
